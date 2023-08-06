3a533d04ecb1f3d4cdab9c6308c0f65972e6bc95
preetlakra <lakraaman007@gmail.com>
2023-08-07 00:56:50 +0530

    Done  Goodnight :) my eyes need rest now

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,14 +14,56 @@ const RenderCards = ({ data, title }) => {
   }
 
 
+
 const Home = () => {
   const [Loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState(null);
 
   const [searchText, setSearchText] = useState(" ");
+  const [SearchedResults, setSearchedResults] = useState(null);
+  
+
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchText);
+    setSearchText(e.target.value);
+
+    setSearchText(
+      setTimeout(() => {
+        const searchResult = allPosts.filter((item) => item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase()));
+        searchResult(searchResult);
+      }, 500),
+    );
+  };
+
 
   return (
-    <section className='mac-w-7xl mc-auto'>
+    <section className='mac-w-7xl mx-auto'>
       <div>
         <h1 className=" font-extrabold text-[#222328] text-[32px]">
           The Community Showcase</h1>
@@ -47,12 +89,12 @@ const Home = () => {
             <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
               {searchText  ?(            
                 <RenderCards
-                  data={[]}
+                  data={allPosts}
                   title="No search results found"
                 />
               ) : (
                 <RenderCards 
-                  data={[]}
+                  data={allPosts}
                   title="No posts found"
                 />
               )}
