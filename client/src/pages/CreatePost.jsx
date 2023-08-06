99f8a4476d1ae13bcbf658890c275084b031acc8
preetlakra <lakraaman007@gmail.com>
2023-08-06 12:55:33 +0530

    update

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -16,18 +16,25 @@ const CreatePost = () => {
   });
 
   const [generatingImg, setGeneratingImg] = useState(false);
-  const [laoding, setLaoding] = useState(false);
+  const [loading, setLaoding] = useState(false);
+
+  const generateImage = () =>{
+    
+
+  }
 
   const handleSubmit = () => {
 
   }
 
-  const handleChange = (e) =>{
-
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
   }
 
-  const handleSurpriseMe = () =>{
 
+  const handleSurpriseMe = () =>{
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
   }
 
   return (
@@ -41,29 +48,70 @@ const CreatePost = () => {
     <form className='mt-16 max-w-3xl' onSubmit={handleSubmit}>
       <div className='flex flex-col gap-5'>
 
-        <FormField 
-          LabelName="Your name"
-          type="text"
-          name="name"
-          placeholder="Ex.. John Doe"
-          value={form.name}
-          handleChange={handleChange}
+        <FormField
+            labelName="Your Name"
+            type="text"
+            name="name"
+            placeholder="Ex., john doe"
+            value={form.name}
+            handleChange={handleChange}
+          />
 
-        />
-        <FormField 
-          LabelName="prompt"
-          type="text"
-          name="prompt"
-          placeholder="A realisitc photograph of a young woman with blue eyes and blonde hair"
-          value={form.prompt}
-          handleChange={handleChange}
-          isSupriseMe
-          handleSurpriseMe={handleSurpriseMe}
-        />
+          <FormField
+              labelName="Prompt"
+              type="text"
+              name="prompt"
+              placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
+              value={form.prompt}
+              handleChange={handleChange}
+              isSurpriseMe
+              handleSurpriseMe={handleSurpriseMe}
+            />
 
+            <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+              { form.photo ? (
+                <img
+                  src={form.photo}
+                  alt={form.prompt}
+                  className="w-full h-full object-contain"
+                />
+              ) : (
+                <img
+                  src={preview}
+                  alt="preview"
+                  className="w-9/12 h-9/12 object-contain opacity-40"
+                />
+              )}
+
+              {generatingImg && (
+                <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                  <Loader />
+                </div>
+              )}
+          </div>
       </div>
-    </form>
+      
+      <div className="mt-5 flex gap-5">
+          <button
+            type="button"
+            onClick={generateImage}
+            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
 
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">** Once you have created the image you want, you can share it with others in the community **</p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? 'Sharing...' : 'Share with the Community'}
+          </button>
+        </div>
+
+    </form>
     </section>
   )
 }
